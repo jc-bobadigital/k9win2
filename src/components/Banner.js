3ed81fda8dd8ca4df1709faa8011ea2287c51848
Chriscel Valerio <chriscelvalerio@Chriscels-MacBook-Pro.local>
2024-09-25 11:23:15 +0800

    Update layout

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,6 +57,9 @@ export const Banner = () => {
             </div>
             <Container>
                 <Row className="align-items-center w-100">
+                    <Col className="column-2" xs={12} md={6} xl={5}>
+                        <Lottie animationData={animationData} />
+                    </Col>
                     <Col className="column-1 text-center" xs={12} md={6} xl={7}>
                         <h1 className="promo-text">
                             <div>100%</div>
@@ -64,9 +67,6 @@ export const Banner = () => {
                             <div className="sub-promo-text">x25 Rollover</div>
                         </h1>
                         <Button href="https://k9inr1.com/en/promotion" variant="warning" size="lg">Play Now and Win <ArrowRightCircle size={25} /></Button>
-                    </Col>
-                    <Col className="column-2" xs={12} md={6} xl={5}>
-                        <Lottie animationData={animationData} />
                     </Col>
                 </Row>
             </Container>
