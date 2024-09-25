0f9f7c721aa2308a394d581371c1a5562e89ef90
Chriscel Valerio <chriscelvalerio@Chriscels-MacBook-Pro.local>
2024-09-26 01:17:02 +0800

    Update image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ import { Row, Col, Container, Button } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
 import Lottie from "lottie-react"
 import animationData from "../assets/json/casino1.json"
+import girlImg from "../assets/img/girl.png"
 
 export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
@@ -55,11 +56,9 @@ export const Banner = () => {
                 <div className="confetti"></div>
                 <div className="confetti"></div>
             </div>
+            <Lottie animationData={animationData} />
             <Container>
                 <Row className="align-items-center w-100">
-                    <Col className="column-2" xs={12} md={6} xl={5}>
-                        <Lottie animationData={animationData} />
-                    </Col>
                     <Col className="column-1 text-center" xs={12} md={6} xl={7}>
                         <h1 className="promo-text">
                             <div>100%</div>
@@ -67,6 +66,9 @@ export const Banner = () => {
                             <div className="sub-promo-text">x25 Rollover</div>
                         </h1>
                         <Button href="https://k9inr1.com/en/promotion" variant="warning" size="lg">Play Now and Win <ArrowRightCircle size={25} /></Button>
+                    </Col>
+                    <Col className="column-2" xs={12} md={6} xl={5}>
+                        <img className="bounce" src={girlImg} alt="Girl" />
                     </Col>
                 </Row>
             </Container>
